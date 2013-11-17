d23bc5f108772c8f711a8e4148ce63edda6cf710
Carlo Ledesma <carlo.miguel.ledesma@gmail.com>
2013-11-17 12:44:30 +0800

    Made use of . to update Angular on callback

--- main.js
+++ main.js
@@ -1,59 +1,66 @@
 
 //-- Start Framework Code
 
-function httpGet(url, callback){
+function HttpUtil() {
 
-	console.log("-- httpGet() --");
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", url, true);
-	xmlHttp.onreadystatechange = function(){
-		processResponse(xmlHttp, callback);
-	}
-	xmlHttp.send(null);
-}
+	this.httpGet = function(url, callback){
 
-function processResponse(xmlHttp, callback){
+		console.log("-- httpGet() --");
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open("GET", url, true);
+		xmlHttp.onreadystatechange = function(){
+			var httpUtil = new HttpUtil();
+			httpUtil.processResponse(xmlHttp, callback);
+		}
+		xmlHttp.send(null);
+	};
 
-	console.log("-- processResponse() --");
-	if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
-		var response = xmlHttp.responseText;
-		console.log("Success. Response Text: " + response);
-		var jsonResponse = getJson(response);
-		callback(jsonResponse);
-	} else {
-		console.log("HTTP Status: " + xmlHttp.status + " Ready state: " + xmlHttp.readyState + " Response Text: " + xmlHttp.responseText);
-	}
-}
+	this.processResponse = function(xmlHttp, callback){
 
-function getJson(response){
-	
-	console.log("-- getJson() --");
-	var jsonResponse = null;
-	if (response != null){
-		jsonResponse = eval('(' + response + ')');
-	} else {
-		console.log("Response is null");
-	}
-	return jsonResponse;
+		console.log("-- processResponse() --");
+		if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
+			var response = xmlHttp.responseText;
+			console.log("Success. Response Text: " + response);
+			var httpUtil = new HttpUtil();
+			var jsonResponse = httpUtil.getJson(response);
+			callback(jsonResponse);
+		} else {
+			console.log("HTTP Status: " + xmlHttp.status + " Ready state: " + xmlHttp.readyState + " Response Text: " + xmlHttp.responseText);
+		}
+	};
+
+	this.getJson = function(response){
+		
+		console.log("-- getJson() --");
+		var jsonResponse = null;
+		if (response != null){
+			jsonResponse = eval('(' + response + ')');
+		} else {
+			console.log("Response is null");
+		}
+		return jsonResponse;
+	};
+
 }
 
 //----- End Framework Code
 
 function HttpController($scope){
 
-	$scope.data = null;
-
 	function callback(json){
 		console.log("-- callback() --");
 		var name = json[0].firstName;
 		console.log("Name: " + name);
-		$scope.data = {name: name};
+		$scope.$apply(function(){
+			$scope.data = {name: name};
+		});
 	}
 
 	$scope.doButtonClick = function(){
 		console.log("-- doButtonClick() --");
-		httpGet("http://localhost:8081/base-jersey-project/user",callback);
-	}
+		var httpUtil = new HttpUtil();
+		httpUtil.httpGet("http://localhost:8081/base-jersey-project/user",callback);
+	}                 
 }
 
 function FirstController($scope){
